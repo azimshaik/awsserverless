a3fc24e2de35e9c768a04e5d65cde7e622349ee6
azimshaik <cheer.buddu@gmail.com>
2019-09-06 00:11:11 -0400

    refractured to respond to API Gateway put request

--- putIem.js
+++ putIem.js
@@ -1,23 +1,45 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
-AWS.config.update({region:"us-east-1"});
+AWS.config.update({ region: "us-east-1" });
 
-exports.handler = async(event, context) => {
-    const ddb = new AWS.DynamoDB({apiVersion: "2012-10-08"});
-    const documentClient = new AWS.DynamoDB.DocumentClient({region:"us-east-1"});
-    
-    const params = {
-        TableName: "users",
-        Item:{
-            id:'1236',
-            firstname: 'Ameem',
-            last:'Shaik'
-        }
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+  const documentClient = new AWS.DynamoDB.DocumentClient({
+    region: "us-east-1"
+  });
+
+  let responseBody = "";
+  let statusCode = 0;
+
+  //descructuring
+  const { id, firstname, lastname } = JSON.parse(event.body);
+
+  const params = {
+    TableName: "users",
+    Item: {
+      id: id,
+      firstname: firstname,
+      lastname: lastname
     }
-    try{
-        const data = await documentClient.put(params).promise();
-        console.log(data);
-    }catch(err){
-        console.log(err);
-    }
-}
+  };
+
+  try {
+    const data = await documentClient.put(params).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 201;
+  } catch (err) {
+    console.log(err);
+    responseBody = "Unable to put user data";
+    statusCode = 403;
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      myheader: "test"
+    },
+    body: responseBody
+  };
+
+  return response;
+};
