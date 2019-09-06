aca95523114a832be588358e8d2495ac836d7934
azimshaik <cheer.buddu@gmail.com>
2019-09-05 23:27:14 -0400

    response object, event.pathParameters id destructure, in response to API Gateway

--- getItem.js
+++ getItem.js
@@ -1,23 +1,42 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
-AWS.config.update({region:"us-east-1"});
+AWS.config.update({ region: "us-east-1" });
 
-exports.handler = async(event, context) => {
-    const ddb = new AWS.DynamoDB({apiVersion: "2012-10-08"});
-    const documentClient = new AWS.DynamoDB.DocumentClient({region:"us-east-1"});
-    
-    const params = {
-        TableName: "users",
-        Item:{
-            id:'1236',
-            firstname: 'Ameem',
-            last:'Shaik'
-        }
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+  const documentClient = new AWS.DynamoDB.DocumentClient({
+    region: "us-east-1"
+  });
+
+  let responseBody = "";
+  let statusCode = 0;
+
+  //descructuring
+  const { id } = event.pathParameters;
+
+  const params = {
+    TableName: "users",
+    Key: {
+      id: id
     }
-    try{
-        const data = await documentClient.put(params).promise();
-        console.log(data);
-    }catch(err){
-        console.log(err);
-    }
-}
+  };
+  try {
+    const data = await documentClient.get(params).promise();
+    responseBody = JSON.stringify(data.Item);
+    statusCode = 200;
+  } catch (err) {
+    console.log(err);
+    responseBody = "Unable to get user data";
+    statusCode = 403;
+  }
+
+  const response = {
+    statusCode: statusCode,
+    headers: {
+      myheader: "test"
+    },
+    body: responseBody
+  };
+
+  return response;
+};
